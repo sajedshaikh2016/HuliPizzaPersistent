aa9a2b5f2b3a12b8ba5e6d880da8e50ba2c2485d
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2024-07-31 12:39:58 +0530

    Initial Commit

---
+++ HuliPizzaPersistent/HuliPizzaPersistentApp.swift
@@ -0,0 +1,17 @@
+//
+//  HuliPizzaPersistentApp.swift
+//  HuliPizzaPersistent
+//
+//  Created by Sajed Shaikh on 31/07/24.
+//
+
+import SwiftUI
+
+@main
+struct HuliPizzaPersistentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
