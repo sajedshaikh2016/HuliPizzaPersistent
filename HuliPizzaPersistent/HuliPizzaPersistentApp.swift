be39826dfa745af03baa03e817282cdeb5e5d04d
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2024-07-31 12:50:37 +0530

    Add a Model

--- HuliPizzaPersistent/HuliPizzaPersistentApp.swift
+++ HuliPizzaPersistent/HuliPizzaPersistentApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct HuliPizzaPersistentApp: App {
@@ -13,5 +14,6 @@ struct HuliPizzaPersistentApp: App {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: [RatingModel.self])
     }
 }
---
+++ HuliPizzaPersistent/Models/RatingModel.swift
@@ -0,0 +1,20 @@
+//
+//  RatingModel.swift
+//  HuliPizzaPersistent
+//
+//  Created by Sajed Shaikh on 31/07/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class RatingModel: Identifiable {
+    var pizzaName: String
+    var rating: Int
+    
+    init(pizzaName: String = "", rating: Int = 0) {
+        self.pizzaName = pizzaName
+        self.rating = rating
+    }
+}
